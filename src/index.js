1fbd40196655850f8fb6924964c10e461cb392d1
ira089 <gvir2015@gmail.com>
2023-11-28 22:13:03 +0200

    js11_4

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 // import SimpleLightbox from 'simplelightbox';
 // import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log('чаокакаm');
-
 const refs = {
   form: document.querySelector('.search-form'),
   btnSearch: document.querySelector('button'),
@@ -13,12 +11,8 @@ const refs = {
   endImg: document.querySelector('.end-img'),
   totalImgs: document.querySelector('.found-total-img'),
 };
-let caunter = 11;
+let caunter = 1;
 let searchImg = '';
-
-// console.log(refs.form);
-// console.log(refs.btnSearch);
-console.log(refs.endImg);
 
 refs.form.addEventListener('submit', imagesSabmit);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
@@ -26,25 +20,25 @@ refs.loadMoreBtn.addEventListener('click', onLoadMore);
 // -----------функция сабмита--------
 async function imagesSabmit(evt) {
   evt.preventDefault();
-  console.log(refs.endImg);
+  // console.log(refs.endImg);
+  refs.loadMoreBtn.hidden = true;
   refs.endImg.classList.add('none-load-more');
-
-  //   const { searchQuery } = evt.currentTarget.elements;
+  refs.totalImgs.classList.add('none-load-more');
   refs.containerImg.innerHTML = '';
-  // caunter = 1;
   searchImg = evt.currentTarget.elements.searchQuery.value;
-  //   const searchImg = searchQuery.value;
-  console.log(searchImg);
+  if (searchImg.trim() === '') {
+    Notify.failure('Sorry, enter text. Please try again.');
+    return;
+  }
+  // console.log(searchImg);
   try {
     const imagesApi = await serviceSearchImg(searchImg);
     const totalImg = imagesApi.totalHits;
+    refs.totalImgs.classList.remove('none-load-more');
     refs.totalImgs.innerHTML = `<p>"Hooray! We found ${totalImg} images."</p>`;
-
-    console.log(imagesApi);
+    // console.log(imagesApi);
     const { hits } = imagesApi;
-    // const qwe = hits[0].tags;
-    // console.log(qwe);
-    console.log(hits);
+    // console.log(hits);
     if (hits.length === 0) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -56,21 +50,18 @@ async function imagesSabmit(evt) {
   } finally {
     refs.loadMoreBtn.hidden = false;
     // evt.target.reset();
-    // refs.formContainer.innerHTML = '<input type="text" name="country" />';
   }
 }
 
 // -----------функция запроса на API--------
 async function serviceSearchImg(search) {
   const BASE_URL = 'https://pixabay.com/api/';
-  //   const ENDPOINT = "forecast.json";
   const API_KEY = '40910000-bc8f7501355e0c431b692ba0e';
 
   const { data } = await axios.get(
     `${BASE_URL}?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${caunter}`
   );
-  // caunter += 1;
-  console.log(data);
+  // console.log(data);
   return data;
 }
 
@@ -106,19 +97,15 @@ function createMarkup(arr) {
     )
     .join('');
 }
+
 //------ функция добавить еще----------
 async function onLoadMore() {
   try {
     caunter += 1;
     const imagesApi = await serviceSearchImg(searchImg);
-
     const { hits } = imagesApi;
-    // const qwe = hits[0].tags;
-    console.log('qwe');
-
     refs.containerImg.insertAdjacentHTML('beforeend', createMarkup(hits));
-
-    console.log(Math.ceil(imagesApi.totalHits / 40));
+    // console.log(Math.ceil(imagesApi.totalHits / 40));
     if (caunter === Math.ceil(imagesApi.totalHits / 40)) {
       refs.loadMoreBtn.hidden = true;
       refs.endImg.classList.remove('none-load-more');
